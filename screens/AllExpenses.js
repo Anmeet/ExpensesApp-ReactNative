8ae096bd13b4f71924184644d1446f52605cfeb5
Amit Bhandari <anmeet619@yahoo.com>
2023-01-16 17:07:49 +1100

    Added all files

---
+++ screens/AllExpenses.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
+import { ExpensesContext } from '../store/expenses-context'
+
+const AllExpenses = () => {
+  const expensesCtx = useContext(ExpensesContext)
+  return (
+    <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod='Total' />
+  )
+}
+
+export default AllExpenses
+
+const styles = StyleSheet.create({})
